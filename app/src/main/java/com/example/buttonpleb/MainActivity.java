584dee08e71e5e2f46d84e7807bfbce427b0451e
Administrator <boss@2hu.com>
2023-10-30 10:46:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/buttonpleb/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.buttonpleb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button click = findViewById(R.id.button);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
